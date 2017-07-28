51de89331a0d08a49ebcc6b5e987c8f8615c6a8d
Ronny Hartenstein <ronny@rh-flow.de>
2017-07-28 23:54:46 +0200

    mit Bildern

--- html.php
+++ html.php
@@ -17,11 +17,23 @@ fwrite($f, "
 <h1>Timon's Blog</h1>
 ");
 
-$db = new SQLite3('timonblog/msgstore.db');
+$db = new SQLite3('source/msgstore.db');
 $id = '4915159161040-1470252332@g.us';
+
+/*
+$res = $db->query('
+  SELECT *, rowid "NAVICAT_ROWID" FROM "main"."message_thumbnails" 
+  WHERE "key_remote_jid" = \''.$id.'\'
+');
+$imgs = [];
+while ($img = $res->fetchArray()) {
+  $imgs[$img['key_id']] = 
+}
+*/
+
 $res = $db->query('
   SELECT * FROM "main"."messages" 
-  WHERE "key_remote_jid" = \''.$id.'\' 
+  WHERE "key_remote_jid" = \''.$id.'\'
   ORDER BY "received_timestamp" ASC
 ');
 while ($msg = $res->fetchArray()) {
@@ -45,19 +57,24 @@ while ($msg = $res->fetchArray()) {
 
   if ($msg['media_mime_type'] == 'image/jpeg') {
     print "*";
-    fwrite($f, "<br/>Bild: ".$msg['media_size']);
-    if (!empty($msg['media_caption'])) {
-      fwrite($f, " - ".$msg['media_caption']);
-    }
-  }
-    //.(!empty($msg['media_name']) ? ' Bild '.$msg['media_name'] : '')
-  /*return '
+    if (preg_match('#(Media/WhatsApp Images/.+?\.jpg)#m', $msg[15], $m)) {
+      //print "\n".$m[1];
+      fwrite($f, '
+<div class="row">
 <div class="col-sm-6 col-md-4">
   <div class="thumbnail">
-    <img src="'.$m[1].'" alt="'.($m[2] ?: '').'">
-    '.(!empty($m[2]) ? $m[2] : '<div class="caption">'.$m[2].'</div>').'
+    <img src="/'.$m[1].'" alt="'.($msg['media_caption'] ?: '').'">
+    '.(!empty($msg['media_caption']) ? '<div class="caption">'.$msg['media_caption'].'</div>' : '').'
   </div>
-</div>';*/
+</div></div>');
+    }
+    /*if (!empty($msg['media_caption'])) {
+      fwrite($f, " - ".$msg['media_caption']);
+    }*/
+  }
+    //.(!empty($msg['media_name']) ? ' Bild '.$msg['media_name'] : '')
+
+  //fwrite($f, '<xmp>'.var_export($msg, 1).'</xmp>');
 }
 $res->finalize();
 $db->close();
